713399b8505dbb7c61a83310877d42231e2f0a7e
Dani47 <87781904+Danii47@users.noreply.github.com>
2024-06-10 16:27:04 +0200

    feat: adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
-  output: "server"
+  output: "server",
+  adapter: vercel()
 });

a3b2617d18fa18f9f907884c6e36dfcc0924d986
Dani47 <87781904+Danii47@users.noreply.github.com>
2024-06-13 22:41:24 +0200

    feat: operation logic

--- src/components/OperationResults.tsx
+++ src/components/OperationResults.tsx
@@ -1,10 +1,23 @@
 import { useEffect, useState } from "preact/hooks"
-import type { BasesValues, Operation, OptionsType } from "../types/conversorTypes"
+import { BASES } from "../constants/Bases"
+import { twoComplementValue } from "../utils/twoComplement"
+import { comprobateValidNumber } from "../utils/comprobateIsValidNumber"
+
+import type { BasesValues, Operation, OptionsType, Base } from "../types/conversorTypes"
+
 import "./Components.css"
+import type { JSX } from "preact/jsx-runtime"
+
+const OPERATIONS = {
+  "+": (firstValue: number, secondValue: number) => firstValue + secondValue,
+  "-": (firstValue: number, secondValue: number) => firstValue - secondValue,
+  "×": (firstValue: number, secondValue: number) => firstValue * secondValue,
+  "÷": (firstValue: number, secondValue: number) => Math.floor(firstValue / secondValue),
+}
 
 export default function OperationResults(
   { firstNumberBaseValues, secondNumberBaseValues, options, operation }:
-  { firstNumberBaseValues: BasesValues, secondNumberBaseValues: BasesValues, options: OptionsType, operation: Operation }
+    { firstNumberBaseValues: BasesValues, secondNumberBaseValues: BasesValues, options: OptionsType, operation: Operation }
 ) {
 
   const [result, setResult] = useState({
@@ -15,45 +28,83 @@ export default function OperationResults(
       10: "0",
       16: "0",
     }
-  
+
   })
 
   useEffect(() => {
-    if (operation === "+") {
-      
+    if (!comprobateValidNumber(firstNumberBaseValues.values["10"], "10") || !comprobateValidNumber(secondNumberBaseValues.values["10"], "10")) return
+
+    const firstValueDecimal = parseInt(firstNumberBaseValues.values["10"], 10)
+    const secondValueDecimal = parseInt(secondNumberBaseValues.values["10"], 10)
+
+    const decimalResult = OPERATIONS[operation](firstValueDecimal, secondValueDecimal)
+
+    if (isNaN(decimalResult) || decimalResult === Infinity) return
+
+    let binaryResult = decimalResult.toString(2)
+
+    if (options.ca2) {
+      binaryResult = binaryResult[0] === "-" ? twoComplementValue("0" + binaryResult.substring(1)) : "0" + binaryResult
+    } else if (options.magnitudSign) {
+      binaryResult = binaryResult[0] === "-" ? "1" + binaryResult.substring(1) : "0" + binaryResult
     }
-  }, [firstNumberBaseValues, secondNumberBaseValues])
+
+
+    localStorage.setItem('data', JSON.stringify({
+      firstNumberBaseValues,
+      secondNumberBaseValues,
+      options,
+    }))
+
+    setResult({
+      bits: binaryResult.length,
+      values: {
+        2: binaryResult.slice(0, 16),
+        8: parseInt(binaryResult, 2).toString(8).slice(0, 16),
+        10: decimalResult.toString(10),
+        16: parseInt(binaryResult, 2).toString(16),
+      }
+    })
+
+  }, [firstNumberBaseValues, secondNumberBaseValues, operation])
+
+  const handleClickResult = (event: JSX.TargetedEvent<HTMLDivElement>) => {
+    const { innerText } = event.target as HTMLDivElement
+    navigator.clipboard.writeText(innerText)
+  }
 
   return (
     <>
-      <h2 class="resultsTitle">Resultados</h2>  
+      <h2 class="resultsTitle">Resultados</h2>
       <div class="resultsContainer">
-        <section>
-          <h2 class="h2Titles">Binario</h2>
-          <div class="resultContainer">
-            {result.values["2"]}
-          </div>
-        </section>
-        <section>
-          <h2 class="h2Titles">Octal</h2>
-          <div class="resultContainer">
-            1011101
-          </div>
-        </section>
-        <section>
-          <h2 class="h2Titles">Decimal</h2>
-          <div class="resultContainer">
-            1011101
-          </div>
-        </section>
-        <section>
-          <h2 class="h2Titles">Hexadecimal</h2>
-          <div class="resultContainer">
-            1011101
-          </div>
-        </section>
-      </div>
+        {
+          Object.entries(BASES).map(([base, name]) => {
 
+            const showSteps = (base === "2") && (operation === "×" || operation === "÷") && result.values[base as Base] !== "0"
+
+            return (
+              <section>
+                {
+                  showSteps ? (
+                    <a href={`${operation === "×" ? "/multiplicacion" : "/division"}`} class="noLinkStyle">
+                      <h2 title="Pasos de procesador" class="h2Titles canClickResult">
+                        {name} <sub>({base})</sub> {(base === "2") ? <span class="bitsNumber">({result.bits} bit{result.bits !== 1 ? "s" : ""})</span> : ""}
+                      </h2>
+                    </a>
+                  ) : (
+                    <h2 class="h2Titles">
+                      {name} <sub>({base})</sub> {(base === "2") ? <span class="bitsNumber">({result.bits} bit{result.bits !== 1 ? "s" : ""})</span> : ""}
+                    </h2>
+                  )
+                }
+                <div onClick={handleClickResult} title="Copiar resultado" class={`resultContainer ${result.values[base as Base] !== "0" ? "showingResult" : ""}`}>
+                  {result.values[base as Base]}
+                </div>
+              </section>
+            )
+          })
+        }
+      </div>
     </>
   )
 }
